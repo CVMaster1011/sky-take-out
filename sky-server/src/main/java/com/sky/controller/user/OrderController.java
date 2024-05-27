2c4404bf99b5ca414f9b0e3fe4ac080df17aa582
longlong <1910354889@qq.com>
2024-05-27 22:17:43 +0800

    用户下单功能代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,13 +1,12 @@
 package com.sky.controller.user;
 
-import com.sky.dto.OrdersCancelDTO;
-import com.sky.dto.OrdersConfirmDTO;
-import com.sky.dto.OrdersPageQueryDTO;
-import com.sky.dto.OrdersRejectionDTO;
+import com.sky.dto.OrdersPaymentDTO;
+import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
-import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -15,50 +14,65 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * 订单管理
- */
-@RestController("adminOrderController")
-@RequestMapping("/admin/order")
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Api(tags = "用户端订单相关接口")
 @Slf4j
-@Api(tags = "订单管理接口")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
     /**
-     * 订单搜索
-     *
-     * @param ordersPageQueryDTO
+     * 用户下单
+     * @param ordersSubmitDTO
      * @return
      */
-    @GetMapping("/conditionSearch")
-    @ApiOperation("订单搜索")
-    public Result<PageResult> conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO) {
-        PageResult pageResult = orderService.conditionSearch(ordersPageQueryDTO);
+    @PostMapping("/submit")
+    @ApiOperation("用户下单")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
+        log.info("用户下单，参数为：{}",ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    @ApiOperation("订单支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        log.info("订单支付：{}", ordersPaymentDTO);
+        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+        log.info("生成预支付交易单：{}", orderPaymentVO);
+        return Result.success(orderPaymentVO);
+    }
+
+    /**
+     * 历史订单查询
+     *
+     * @param page
+     * @param pageSize
+     * @param status   订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消
+     * @return
+     */
+    @GetMapping("/historyOrders")
+    @ApiOperation("历史订单查询")
+    public Result<PageResult> page(int page, int pageSize, Integer status) {
+        PageResult pageResult = orderService.pageQuery4User(page, pageSize, status);
         return Result.success(pageResult);
     }
 
     /**
-     * 各个状态的订单数量统计
-     *
-     * @return
-     */
-    @GetMapping("/statistics")
-    @ApiOperation("各个状态的订单数量统计")
-    public Result<OrderStatisticsVO> statistics() {
-        OrderStatisticsVO orderStatisticsVO = orderService.statistics();
-        return Result.success(orderStatisticsVO);
-    }
-
-    /**
-     * 订单详情
+     * 查询订单详情
      *
      * @param id
      * @return
      */
-    @GetMapping("/details/{id}")
+    @GetMapping("/orderDetail/{id}")
     @ApiOperation("查询订单详情")
     public Result<OrderVO> details(@PathVariable("id") Long id) {
         OrderVO orderVO = orderService.details(id);
@@ -66,62 +80,39 @@ public class OrderController {
     }
 
     /**
-     * 接单
+     * 用户取消订单
      *
      * @return
      */
-    @PutMapping("/confirm")
-    @ApiOperation("接单")
-    public Result confirm(@RequestBody OrdersConfirmDTO ordersConfirmDTO) {
-        orderService.confirm(ordersConfirmDTO);
-        return Result.success();
-    }
-
-    /**
-     * 拒单
-     *
-     * @return
-     */
-    @PutMapping("/rejection")
-    @ApiOperation("拒单")
-    public Result rejection(@RequestBody OrdersRejectionDTO ordersRejectionDTO) throws Exception {
-        orderService.rejection(ordersRejectionDTO);
-        return Result.success();
-    }
-
-    /**
-     * 取消订单
-     *
-     * @return
-     */
-    @PutMapping("/cancel")
+    @PutMapping("/cancel/{id}")
     @ApiOperation("取消订单")
-    public Result cancel(@RequestBody OrdersCancelDTO ordersCancelDTO) throws Exception {
-        orderService.cancel(ordersCancelDTO);
+    public Result cancel(@PathVariable("id") Long id) throws Exception {
+        orderService.userCancelById(id);
         return Result.success();
     }
 
     /**
-     * 派送订单
+     * 再来一单
      *
+     * @param id
      * @return
      */
-    @PutMapping("/delivery/{id}")
-    @ApiOperation("派送订单")
-    public Result delivery(@PathVariable("id") Long id) {
-        orderService.delivery(id);
+    @PostMapping("/repetition/{id}")
+    @ApiOperation("再来一单")
+    public Result repetition(@PathVariable Long id) {
+        orderService.repetition(id);
         return Result.success();
     }
 
     /**
-     * 完成订单
-     *
+     * 客户催单
+     * @param id
      * @return
      */
-    @PutMapping("/complete/{id}")
-    @ApiOperation("完成订单")
-    public Result complete(@PathVariable("id") Long id) {
-        orderService.complete(id);
+    @GetMapping("/reminder/{id}")
+    @ApiOperation("客户催单")
+    public Result reminder(@PathVariable("id") Long id){
+        orderService.reminder(id);
         return Result.success();
     }
 }
